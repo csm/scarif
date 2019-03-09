1b538f3b60fbd5ebe98a8ec3eaed3625e7450c18
Casey Marshall <casey.s.marshall@gmail.com>
2019-03-08 19:20:24 -0800

    Version 0.1.4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.csm/scarif "0.1.3"
+(defproject com.github.csm/scarif "0.1.4-SNAPSHOT"
   :description "Dynamic configuration library"
   :url "https://github.com/csm/scarif"
   :license {:name "Eclipse Public License"
