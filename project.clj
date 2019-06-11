f229686e4c329d1f0691a468eb18f306e3e73c0b
Casey Marshall <csm@noonhome.com>
2019-06-11 11:25:12 -0700

    Version 0.1.6

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.csm/scarif "0.1.6-SNAPSHOT"
+(defproject com.github.csm/scarif "0.1.6"
   :description "Dynamic configuration library"
   :url "https://github.com/csm/scarif"
   :license {:name "Eclipse Public License"
