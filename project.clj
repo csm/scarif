0014c6cc6b1d79568c245b03c34067e806de789d
Casey Marshall <csm@noonhome.com>
2019-11-25 11:37:22 -0800

    Clojure 1.10.1.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/csm/scarif"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [com.netflix.archaius/archaius-core "0.7.4"]
                  [com.netflix.archaius/archaius-aws "0.7.4" :exclusions [commons-logging joda-time]]
                  [joda-time "2.10"]]
